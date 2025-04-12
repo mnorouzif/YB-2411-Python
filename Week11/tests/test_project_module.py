71cefc6bf95d86516cdc8cd3326de739922ba393
Mohammad <mnorouzif@gmail.com>
2025-04-13 10:02:53 +1200

    update files
    Initial changes

--- Week11/tests/test_project_module.py
+++ Week11/tests/test_project_module.py
@@ -1,22 +1,21 @@
 import pytest
 
-from my_project import project_module
-
+import project_module
 
 def test_something(my_data):
-    assert my_data == 42
+    assert my_data == 41
 
 @pytest.mark.parametrize("values,expected_results",[
     (
     [1,2,3,4,5,6],
-    [2.0, 3.0, 4.0, 5.0],
+    [3,4 ],
     ),
     (
     [-1,-2,-3,-4,-5,-6],
-    [-2.0, -3.0, -4.0 ],
+    [-3.0, -4],
     ),
 ])
 def test_rolling_average(values,expected_results):
 
-    result = project_module.rolling_average(values, 3)
+    result = project_module.rolling_average(values, 5)
     assert result == expected_results
