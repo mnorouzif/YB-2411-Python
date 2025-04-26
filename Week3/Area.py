014bcd8c9df93a2d68fe61150679004584edaee2
Mohammad <mnorouzif@gmail.com>
2025-04-26 12:22:42 +1200

    updated _files_v0.3

---
+++ Week3/Area.py
@@ -0,0 +1,7 @@
+number_l, number_w = input("Input value (L & W): ").split()
+number_l =int(number_l)
+number_w = int(number_w)
+def area(number_l, number_w):
+    Area_lan = number_l* number_w
+    return (Area_lan)
+print(area(number_l,number_w))
