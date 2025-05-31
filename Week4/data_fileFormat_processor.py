d1d97ca41419844e7f8d0e226e8ae684b4b1e3e7
Mohammad <mnorouzif@gmail.com>
2025-05-31 12:01:46 +1200

    Updated file name
    CSV file name is updated

--- Week4/data_fileFormat_processor.py
+++ Week4/data_fileFormat_processor.py
@@ -25,12 +25,14 @@ class DataFileFormat_Processor:
         print("\nDescriptive Statistics:")
         print(self.data.describe())
 
+
 def main():
     
-    file_path = 'Week4/sample_junk_mail.csv' 
+    file_path = 'Week4/sample_junk_mail_updated.csv' 
     processor = DataFileFormat_Processor(file_path)
     processor.load_data()
     processor.initial_processing()
+
 
 if __name__ == "__main__":
     main()
